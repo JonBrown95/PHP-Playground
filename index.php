98701517bf00a74b77d22c3eb85aef4846388181
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2023-12-24 00:32:13 +0000

    Reverted back to index.php

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple PHP Calculator</title>
+    <title>PHP Calculator</title>
 </head>
 
 <body>
-    <h2>Simple PHP Calculator</h2>
+    <h2>PHP Calculator</h2>
     <form method="post" action="">
         <input type="text" name="num1" placeholder="Enter first number">
         <select name="operator">
@@ -22,11 +22,13 @@
     </form>
 
     <?php
+    $num1 = $num2 = $operator = $result = '';
+
     if (isset($_POST['calculate'])) {
         $num1 = $_POST['num1'];
         $num2 = $_POST['num2'];
         $operator = $_POST['operator'];
-        $result = '';
+        
     }
 
     switch ($operator) {
