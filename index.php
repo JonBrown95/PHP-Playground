784f6440148f34435a851f7fd07030957f31839e
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2023-12-24 00:07:02 +0000

    Added HTML form for a simple PHP calculator

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Simple PHP Calculator</title>
 </head>
+
 <body>
-    
+    <h2>Simple PHP Calculator</h2>
+    <form method="post" action="">
+        <input type="text" name="num1" placeholder="Enter first number">
+        <select name="operator">
+            <option value="add">+</option>
+            <option value="subtract">-</option>
+            <option value="multiply">*</option>
+            <option value="divide">/</option>
+        </select>
+        <input type="text" name="num2" placeholder="Enter second number">
+        <input type="submit" name="calculate" value="Calculate">
+    </form>
 </body>
+
 </html>
